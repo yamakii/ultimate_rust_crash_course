587594b669a37bf237cafce0653ccfa0cade5976
yamaki <yamaki@wh-plus.com>
2021-06-09 21:43:21 +0900

    finish section 4

--- out/production/ultimate_rust_crash_course.git/main.rs
+++ out/production/ultimate_rust_crash_course.git/main.rs
@@ -20,11 +20,6 @@ fn main() {
         // - If arg is "double", then call the double() function
         // - If arg is anything else, then call the count() function, passing "arg" to it.
         if arg == "sum" {
-            sum();
-        } else if arg == "double" {
-            double();
-        } else {
-            count(arg);
         }
 
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
@@ -37,9 +32,6 @@ fn sum() {
     // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
-    for i in 7..=23 {
-        sum += i;
-    }
 
 
     println!("The sum is {}", sum);
@@ -49,15 +41,11 @@ fn double() {
     let mut count = 0;
     let mut x = 1;
     // 3. Use a "while loop" to count how many times you can double the value of `x` (multiply `x`
-    // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
+    // by 2) before it is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
-    while x < 500 {
-        count += 1;
-        x *= 2;
-    }
 
 
-    println!("You can double x {} times until x is larger than 500", count);
+    println!("You can double x {} times before it is larger than 500", count);
 }
 
 fn count(arg: String) {
@@ -65,14 +53,7 @@ fn count(arg: String) {
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
     // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
-    let mut count = 0;
-    loop {
-        if count >= 8 {
-            break;
-        }
-        print!("{}", arg);
-        count +=1;
-    }
+
 
     println!(); // This will output just a newline at the end for cleanliness.
 }
